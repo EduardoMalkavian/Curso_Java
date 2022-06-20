7992b220f9c83fc46415bb153e42a2df30a3db6c
EduardoMalkavian <eduardoaraujo33@gmail.com>
2022-06-20 20:35:47 -0300

    Loop FOR

---
+++ Curso_Basico_Dev/src/com/eduardo/cursojava/aula8/For.java
@@ -0,0 +1,17 @@
+package com.eduardo.cursojava.aula8;
+
+public class For {
+
+	public static void main(String[] args) {
+		
+		for (int i=0;i < 5;i++) {
+			System.out.println("i tem valor:" + i);
+		}
+		
+		for (int i= 0, j = 10; i <= j ;i++, j--) {
+			System.out.println("i tem valor:" + i  +" "+ "J tem valor:" + j);
+			
+		}
+	}
+
+}
