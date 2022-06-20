6811e89d97dd81fb818fd56d81f90810e4fb2b20
EduardoMalkavian <eduardoaraujo33@gmail.com>
2022-06-20 20:10:17 -0300

    While

---
+++ Curso_Basico_Dev/src/com/eduardo/cursojava/aula7/LoopWhile.java
@@ -0,0 +1,19 @@
+package com.eduardo.cursojava.aula7;
+
+public class LoopWhile {
+
+	public static void main(String[] args) {
+		
+		int i = 1;
+		int max = 10;
+		
+		System.out.println("contanto até 10" + max);
+		
+		while (i <= max) {
+			System.out.println("O valor de i:" + i);
+			i++;
+		}
+
+	}
+
+}
