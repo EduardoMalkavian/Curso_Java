4c159a1e1c582e77fd426ecd0798f9bc5bc396e2
EduardoMalkavian <eduardoaraujo33@gmail.com>
2022-07-04 19:12:08 -0300

    Classes , Objetos e Atributos

---
+++ Curso_Basico_Dev/src/com/eduardo/cursojava/aula11/Carro.java
@@ -0,0 +1,13 @@
+package com.eduardo.cursojava.aula11;
+
+public class Carro {
+	
+	String marca;
+	String modelo;
+	int numPassageiros; //numero de passageiros
+	double capCombustivel; // capacidade de combsutível
+	double consumoCombustivel; // consumo de combustível
+	
+	
+	
+}
