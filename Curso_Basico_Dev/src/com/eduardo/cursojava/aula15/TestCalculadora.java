3021d00dc797b05699b163168d75bc29c5c9f61d
EduardoMalkavian <eduardoaraujo33@gmail.com>
2022-07-13 18:44:09 -0300

    Sobrecarga de metodos e construtores

---
+++ Curso_Basico_Dev/src/com/eduardo/cursojava/aula15/TestCalculadora.java
@@ -0,0 +1,18 @@
+package com.eduardo.cursojava.aula15;
+
+import java.util.SortedMap;
+
+public class TestCalculadora {
+
+	public static void main(String[] args) {
+	
+			MinhaCalculadora calc = new MinhaCalculadora();
+			
+			calc.soma(1, 2);
+			
+			System.out.println("A soma dos primeiro método da:" );
+			
+	}
+		
+	
+}
